46cb4672351649c84107a8371f697ac44abf7aa0
Omar Ismail <omarfootball2007@outlook.com>
2026-03-22 01:58:37 +0200

    merge

--- src/crdt/block/BlockDLL.java
+++ src/crdt/block/BlockDLL.java
@@ -245,8 +245,14 @@ public class BlockDLL implements ICRDT<BlockNode> {
         automerge(blockID, siteID, time, clock);
     }
     public void replaceChar(String oldCharID, CharNode newChar,int siteID, long clock, long time) {
-        deleteChar(oldCharID, siteID,clock, time);
-        insertChar(oldCharID, newChar);
+        String blockID = charBlockMap.get(oldCharID);
+        deleteChar(oldCharID, siteID, clock, time);
+        if (blockID == null) return;
+        BlockNode block = map.get(blockID);
+        if (block == null || block.isDeleted()) return;
+        block.getContent().insert(newChar);
+        charBlockMap.put(newChar.getCharID(), blockID);
+        autosplit(newChar.getSiteID(), newChar.getClock() + 1, newChar.getTime(), blockID);
     }
 
     public CharDLL copyBlockContent(String blockID, int siteID, long clock, long time, String startCharID) {
