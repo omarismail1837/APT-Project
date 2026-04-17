7192daa2dc7778f037b3444770c4ed7e5543a7c0
Omar Ismail <omarfootball2007@outlook.com>
2026-04-18 01:51:52 +0200

    dependency fix?

--- client/src/main/java/App/WebSocketService.java
+++ client/src/main/java/App/WebSocketService.java
@@ -1,54 +1,13 @@
 package App;
 
-//import App.crdt.action.Action;
+import App.crdt.action.Action;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.*;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
-package App.crdt.action;
 
-public class Action {
-
-    long clock; //used to identify unique actions along with siteID
-    int siteID;
-    String documentID;
-    String actionType;
-    String startCharID;
-    String endCharID;
-    String extraData;
-
-    public Action(long clock, int SiteID, String documentID, String actionType, String startCharID, String endCharID, String extraData) {
-        this.clock = clock;
-        this.siteID = SiteID;
-        this.documentID = documentID;
-        this.actionType = actionType;
-        this.startCharID = startCharID;
-        this.endCharID = endCharID;
-        this.extraData = extraData;
-    }
-    public Action() {} //required for JSON
-    //getters
-    public long getClock() {return clock;}
-    public int getSiteID() {return siteID;}
-    public String getDocumentID() {return documentID;}
-    public String getActionType() {return actionType;}
-    public String getStartCharID() {return startCharID;}
-    public String getEndCharID() {return endCharID;}
-    public String getExtraData() {return extraData;}
-
-    //setters
-    public void setClock(long clock) {this.clock = clock;}
-    public void setSiteID(int siteID) {this.siteID = siteID;}
-    public void setDocumentID(String documentID) {this.documentID = documentID;}
-    public void setActionType(String actionType) {this.actionType = actionType;}
-    public void setStartCharID(String startCharID) {this.startCharID = startCharID;}
-    public void setEndCharID(String endCharID) {this.endCharID = endCharID;}
-    public void setExtraData(String extraData) {this.extraData = extraData;}
-
-
-}
 import java.lang.reflect.Type;
 import java.util.List;
 import java.util.function.Consumer;
--- server/src/main/java/App/ServerMain.java
+++ server/src/main/java/App/ServerMain.java
@@ -3,21 +3,11 @@ package App; // Must match your folder name
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import App.crdt.action.Action;
-import App.crdt.character.CharDLL;
-import App.crdt.block.BlockDLL;
-
 
 @SpringBootApplication
 public class ServerMain {
     public static void main(String[] args) {
         SpringApplication.run(ServerMain.class, args);
     }
-
-    //create blockdll (should be from database later)
-    BlockDLL document = new BlockDLL();
-
-
-
 
 }
