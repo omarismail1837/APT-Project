9d918fa173cd25f57afbe92dbd0490526ffdf859
Omar Ismail <omarfootball2007@outlook.com>
2026-04-25 00:18:29 +0300

    Revert "added code so server works with submitted version"
    This reverts commit 2c6ca61dfb8134d1fa957ee8f9186f0e81cd3bff.

--- server/src/main/java/App/ChatController.java
+++ server/src/main/java/App/ChatController.java
@@ -3,21 +3,16 @@ package App;
 import App.crdt.action.Action;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ChatController {
     private final SimpMessagingTemplate messagingTemplate;
     private final ActionRepository actionRepository; // Use the repository instead
-
-    //remove later
-    private List<Action> allActions = new ArrayList<>();
 
     public ChatController(SimpMessagingTemplate messagingTemplate, ActionRepository actionRepository) {
         this.messagingTemplate = messagingTemplate;
@@ -40,22 +35,5 @@ public class ChatController {
         System.out.println("Sending " + history.size() + " actions");
 
         return history;
-    }
-
-    // code so that it works with version sumbitted -- remove later
-
-    @MessageMapping("/send-data")
-    @SendTo("/topic/updates")
-    public Action sendUpdate(Action update) {
-        System.out.println("Received update: " + update);
-        if (!allActions.contains(update) && !update.getActionType().equals("CURSOR")) allActions.add(update);
-        return update;
-    }
-
-    @SubscribeMapping("/initial-state")
-    public List<Action> sendInitialState() {
-        System.out.println("New user joined. Sending full document state...");
-        // Return the full list of actions
-        return new ArrayList<>(allActions);
     }
 }
