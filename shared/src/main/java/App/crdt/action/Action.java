ea59d2520c58118d98c94a51ff3eeedc5f5bf047
Omar Ismail <omarfootball2007@outlook.com>
2026-04-24 19:45:07 +0300

    action id

--- shared/src/main/java/App/crdt/action/Action.java
+++ shared/src/main/java/App/crdt/action/Action.java
@@ -1,15 +1,24 @@
 package App.crdt.action;
 
+import org.springframework.data.annotation.Id;import org.springframework.data.mongodb.core.index.Indexed;import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
+
+@Document(collection = "actions")
 public class Action {
 
     long clock; //used to identify unique actions along with siteID
     long time;
     int siteID;
+    @Id
+    String id;
+    @Indexed
     String documentID;
     String actionType;
     String startCharID;
     String endCharID;
     String extraData;
+    int colorIndex = -1;
 
     public Action(long clock, long time, int SiteID, String documentID, String actionType, String startCharID, String endCharID, String extraData) {
         this.clock = clock;
@@ -20,6 +29,8 @@ public class Action {
         this.startCharID = startCharID;
         this.endCharID = endCharID;
         this.extraData = extraData; //holds JSON if pasting, character if inserting, true/false if italic/bold
+        this.colorIndex = -1;
+        this.id = getActionId();
     }
     public Action() {} //required for JSON
     //getters
@@ -31,6 +42,8 @@ public class Action {
     public String getStartCharID() {return startCharID;}
     public String getEndCharID() {return endCharID;}
     public String getExtraData() {return extraData;}
+    public int getColorIndex() {return colorIndex;}
+    public String getId() {return id;}
 
     //setters
     public void setClock(long clock) {this.clock = clock;}
@@ -41,6 +54,37 @@ public class Action {
     public void setStartCharID(String startCharID) {this.startCharID = startCharID;}
     public void setEndCharID(String endCharID) {this.endCharID = endCharID;}
     public void setExtraData(String extraData) {this.extraData = extraData;}
+    public void setColorIndex(int colorIndex) {this.colorIndex = colorIndex;}
+
+    public String getActionId() {
+        return documentID + ":" + siteID + ":" + clock;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Action other)) return false;
+        return clock == other.clock
+                && siteID == other.siteID
+                && Objects.equals(documentID, other.documentID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(clock, siteID, documentID);
+    }
+
+    @Override
+    public String toString() {
+        return "Action{" +
+                "id='" + getActionId() + '\'' +
+                ", type='" + actionType + '\'' +
+                ", start='" + startCharID + '\'' +
+                ", end='" + endCharID + '\'' +
+                ", extra='" + extraData + '\'' +
+                ", colorIndex='" + colorIndex + '\'' +
+                '}';
+    }
 
 
 }
